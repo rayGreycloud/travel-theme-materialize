ee7a91cfa022b2ab9ad79f60a54a74e29836bc22
rayGreycloud <rayGreycloud@gmail.com>
2018-04-12 10:19:03 -0700

    Initialize slider

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 // Sidenav 
-const sideNav = document.querySelector('.sidenav');
-
-// Initialize materialize sidenav 
+const sideNav = document.querySelector('.sidenav'); 
 M.Sidenav.init(sideNav, {});
+
+// Slider 
+const slider = document.querySelector('.slider');
+M.Slider.init(slider, {
+  indicators: false,
+  height: 500,
+  transition: 500,
+  interval: 6000
+});
