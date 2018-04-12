41a18dcb9d216f3a835d6657364c7410a20a3622
rayGreycloud <rayGreycloud@gmail.com>
2018-04-12 10:59:16 -0700

    Move js to folder

---
+++ js/index.js
@@ -0,0 +1,26 @@
+// Sidenav 
+const sideNav = document.querySelector('.sidenav'); 
+M.Sidenav.init(sideNav, {});
+
+// Slider 
+const slider = document.querySelector('.slider');
+M.Slider.init(slider, {
+  indicators: false,
+  height: 500,
+  transition: 500,
+  interval: 6000
+});
+
+// Autocomplete 
+const ac = document.querySelector('.autocomplete');
+M.Autocomplete.init(ac, {
+  data: {
+    "Aruba": null,
+    "Mexico": null,
+    "Hawaii": null,
+    "Florida": null,
+    "California": null,
+    "Europe": null
+  }
+});
+
